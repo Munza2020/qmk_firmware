1bc767bc7c6daa9d3fc7f7ce6e7881d44cdc11fc
Munza2020 <mark.l.munro@gmail.com>
2023-09-26 13:23:07 +0100

    changed lighting to work again

--- keyboards/splitkb/kyria/keymaps/Munza2020/rules.mk
+++ keyboards/splitkb/kyria/keymaps/Munza2020/rules.mk
@@ -1,6 +1,6 @@
 OLED_ENABLE = yes
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
-RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LTO_ENABLE = yes
 MOUSEKEY_ENABLE = no
