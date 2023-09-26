63d17f673cd75e59aa905fd9f020bc06a00cd649
Munza2020 <mark.l.munro@gmail.com>
2023-09-26 11:56:34 +0100

    updated keycodes for UK

--- keyboards/splitkb/kyria/keymaps/Munza2020/rules.mk
+++ keyboards/splitkb/kyria/keymaps/Munza2020/rules.mk
@@ -1,5 +1,6 @@
 OLED_ENABLE = yes
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = yes
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LTO_ENABLE = yes
+MOUSEKEY_ENABLE = no
